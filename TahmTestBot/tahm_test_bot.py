3921d43c66691422e5dd240ab0ef093852de5197
Tom Choi <tomchois@gmail.com>
2018-07-28 18:21:37 -0700

    move_cmd_added

--- TahmTestBot/tahm_test_bot.py
+++ TahmTestBot/tahm_test_bot.py
@@ -1,7 +1,8 @@
 import discord
+import random
 from discord.ext import commands
 
-BOT_PREFIX = ('&')
+BOT_PREFIX = '&'
 TOKEN = "NDcxNzkxNDgxNTMxNjYyMzM3.Dj589w.JT1galpWvN67pex0iOldHdcUhps"
 
 E = '.'
@@ -12,17 +13,19 @@ BOARD = [
     [E, E, E],
     [E, E, E]
     ]
+FIRST = X
 
 bot = commands.Bot(command_prefix=BOT_PREFIX)
 
 bot.remove_command('help')
 
+
 @bot.command(pass_context=True)
 async def help(ctx):
-    help_message ='''
+    help_message = '''
 tahm test bot
 
-tahm's testing bot.
+Tom's testing bot.
 
 &hello
     -Says hello.
@@ -32,16 +35,33 @@ tahm's testing bot.
         -All commands (!, ?, &)
         -Any empty messages
         -Any text-formatted messages
+        
+&nick
+    -Picks a random nickname.
+    -(Coming Soon) Sets nickname
+        
+&board
+    -Creates a Tic-Tac-Toe board.
+    
+&move
+    -Places an X or an O on a space.
+    -Format: ROW_NUMBER COL_NUMBER
+        -Example: &board 1 1
+        
+&reset
+    -Resets/Clears the board.
 
 &help
     -Shows this message.'''
-    H = "```" + help_message + "```"
-    await ctx.bot.say(H)
+    help_message_formatted = "```" + help_message + "```"
+    await ctx.bot.say(help_message_formatted)
+
 
 @bot.command(pass_context=True)
 async def hello(ctx):
     author = ctx.message.author
     await ctx.bot.say("Hello " + author.mention)
+
 
 @bot.command(pass_context=True)
 async def purge(ctx):
@@ -50,6 +70,7 @@ async def purge(ctx):
 
     await ctx.bot.purge_from(channel, limit=1000, check=check)    
     await ctx.bot.say("Purge Complete", delete_after=10)
+
 
 def board_display():
     board = f'''
@@ -66,29 +87,60 @@ def board_display():
 '''
     return "```" + board + "```"
 
+
 def clear():
     for row in range(3):
         for col in range(3):
             BOARD[row][col] = E
 
+
 def place(r, c):
-    BOARD[r][c] = X
+    BOARD[r][c] = FIRST
+
+
+def switch():
+    global FIRST
+    if FIRST == X:
+        FIRST = O
+    else:
+        FIRST = X
+
 
 @bot.command(pass_context=True)
 async def board(ctx):
     await ctx.bot.say(board_display())
+
 
 @bot.command(pass_context=True)
 async def reset(ctx):
     clear()
     await ctx.bot.say("Board Reset!")
 
-'''
+
 @bot.command(pass_context=True)
-async def move(ctx):
-    
-    await ctx.bot.say(board())
-'''
+async def move(ctx, *args):
+    r = int(args[0]) - 1
+    c = int(args[1]) - 1
+    if BOARD[r][c] == E:
+        place(r, c)
+        switch()
+    else:
+        await ctx.bot.say(f"Space ({r}, {c}) is already taken!")
+    await ctx.bot.say(board_display())
+
+
+@bot.command(pass_context=True)
+async def nick(ctx):
+    author = ctx.message.author
+    nicks = [
+        "Needed a New Nickname",
+        "Melon Lord",
+        "GOAT"
+    ]
+    new_nick = random.choice(nicks)
+    await ctx.bot.change_nickname(author, new_nick)
+    await ctx.bot.say(f"You have been knighted as {new_nick}, " + author.mention)
+
 
 @bot.event
 async def on_ready():
